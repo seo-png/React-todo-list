044602086240dc7012efad2b096e3f8a8e1f8a69
seo-png <iseohyeon128@gmail.com>
2023-05-14 05:49:07 +0900

    비동기처리 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { atom, selector, useRecoilValue } from 'recoil'
+import axios from 'axios';
 
+const todoIdState = atom({
+  key: 'todoIdState',
+  default: 1
+})
+
+const todoItemQuery = selector({
+  key: 'todoItemQuery',
+  get: async ({ get }) => {
+    const id = get(todoIdState);
+
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
+
+    return response.data;
+  }
+})
 function App() {
+  const data = useRecoilValue(todoItemQuery)
   return (
     <div>
-      Hello Recoil!
+      {data.title}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<p>Loading...</p>}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </Suspense>
+    
   </React.StrictMode>
 );
 
